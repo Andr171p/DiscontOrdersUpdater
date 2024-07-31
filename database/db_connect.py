78518b36abd9b55a1e79b6737ded78b22cf658d1
Tyuiu.KosovAS@outlook.com <andrey.kosov.05@inbox.ru>
2024-08-01 01:57:16 +0500

    1.0.3 updater+Procfile

--- database/db_connect.py
+++ database/db_connect.py
@@ -1,14 +1,14 @@
 import aiomysql
 
 from database.db_loader import create_event_loop
-from database.db_config import AccessDB
+from database.db_config import RailwayAccessDB
 
 
 loop = create_event_loop()
 
 
 async def connect():
-    access_db = AccessDB
+    access_db = RailwayAccessDB
     return await aiomysql.create_pool(
         host=access_db.host,
         port=access_db.port,
